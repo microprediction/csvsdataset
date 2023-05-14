971ea36e2f1c1cc27a431048e63b31619d7b2f0c
Peter Cotton <57455669+microprediction@users.noreply.github.com>
2023-05-14 09:37:50 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="csvsdataset",
-    version="0.0.6",
+    version="0.0.7",
     description="Memory frugal torch dataset from a csv collection",
     long_description=README,
     long_description_content_type="text/markdown",
